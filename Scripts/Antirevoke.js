20f939b8ebb1be25b025b2a0e7229415416d8493
Nguyễn Công Trí <ngoisaophuongdong93@gmail.com>
2020-03-06 23:35:21 +0700

    Update Antirevoke.js

--- Scripts/Antirevoke.js
+++ Scripts/Antirevoke.js
@@ -1,6 +1,6 @@
 var DIRECT = "DIRECT";
 var PROXY = "PROXY 127.0.0.1:80";
-var blacklist = {"ocsp.apple.com":1,"oscp.apple.com":1,"mesu.apple.com":1,"updates-http.cdn-apple.com":1,"appldnld.apple.com":1,"xp.apple.com":1,"ppq.apple.com":1,"gdmf.apple.com":1,"www.gdmf.apple.com":1,"ocsp.int-x3.letsencrypt.org":1,"world-gen.g.aaplimg.com":1,"mobile-api.adguard.com":1,"lichviet.org":1,"apimboom2.globaldelight.net":1,"bmall.camera360.com":1,"api-intl.mr.meitu.com":1,"us-central1-infltr.cloudfunctions.net":1,"api.mixpanel.com":1};
+var blacklist = {"ocsp.apple.com":1,"mesu.apple.com":1,"updates-http.cdn-apple.com":1,"appldnld.apple.com":1,"xp.apple.com":1,"appldnld.apple.com":1,"gdmf.apple.com":1,"ocsp.int-x3.letsencrypt.org":1,"world-gen.g.aaplimg.com":1,"lichviet.org":1};
 function FindProxyForURL(url, host) {
   host = host.toLowerCase();
   for (i = 0; i < 30; i++) {
