18aa2c76325a4ff65e660740c6bb97348aa79259
Nguyễn Công Trí <ngoisaophuongdong93@gmail.com>
2020-03-13 11:45:36 +0700

    Update Pacdebug.js and Antirevoke.js

--- Scripts/Antirevoke.js
+++ Scripts/Antirevoke.js
@@ -1,6 +1,6 @@
 var DIRECT = "DIRECT";
 var PROXY = "PROXY 127.0.0.1:80";
-var blacklist = {"ocsp.apple.com":1,"mesu.apple.com":1,"updates-http.cdn-apple.com":1,"appldnld.apple.com":1,"xp.apple.com":1,"appldnld.apple.com":1,"gdmf.apple.com":1,"ocsp.int-x3.letsencrypt.org":1,"world-gen.g.aaplimg.com":1,"lichviet.org":1};
+var blacklist = {"ocsp.apple.com":1,"mesu.apple.com":1,"updates-http.cdn-apple.com":1,"appldnld.apple.com":1,"xp.apple.com":1,"appldnld.apple.com":1,"gdmf.apple.com":1,"ocsp.int-x3.letsencrypt.org":1,"world-gen.g.aaplimg.com":1,"us-central1-infltr.cloudfunctions.net":1,"api-intl.mr.meitu.com":1,"bmall.camera360.com":1,"mobile-api.adguard.com":1,"lichviet.org":1};
 function FindProxyForURL(url, host) {
   host = host.toLowerCase();
   for (i = 0; i < 30; i++) {
