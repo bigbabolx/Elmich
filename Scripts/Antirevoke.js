9e40f570f06cc64430e49bb714ddda1b8d15ea70
Nguyễn Công Trí <ngoisaophuongdong93@gmail.com>
2020-04-22 22:23:32 +0700

    Update Antirevoke.js

--- Scripts/Antirevoke.js
+++ Scripts/Antirevoke.js
@@ -1,18 +1,18 @@
 var DIRECT = "DIRECT";
 var PROXY = "PROXY 127.0.0.1:80";
-var BLACK = "SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080;";
 var blacklist = {"ocsp.apple.com":1,"mesu.apple.com":1,"updates-http.cdn-apple.com":1,"appldnld.apple.com":1,"xp.apple.com":1,"appldnld.apple.com":1,"gdmf.apple.com":1,"ocsp.int-x3.letsencrypt.org":1,"world-gen.g.aaplimg.com":1,"us-central1-infltr.cloudfunctions.net":1,"api-intl.mr.meitu.com":1,"bmall.camera360.com":1,"mobile-api.adguard.com":1,"lichviet.org":1};
 function FindProxyForURL(url, host) {
-  var lastPos;
-  do {
-     if (blacklist.hasOwnProperty(host)) {
-      return BLACK;
+  host = host.toLowerCase();
+  for (i = 0; i < 30; i++) {
+    if (blacklist[host]) {
+      return PROXY;
     }
-    lastPos=
-    host.indexOf(‘.’) + 1;
-    host =
-    host.slice(lastPos);
+    var index = host.indexOf(".");
+    if (index == -1) {
+      break;
+    } else {
       host = host.substring(index + 1);
-    } while (lastPos >= 1);
+    }
+  }
   return DIRECT;
 }
